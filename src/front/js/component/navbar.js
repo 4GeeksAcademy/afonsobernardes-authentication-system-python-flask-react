ec4e694787986fe9e438cac5ea23dbdb4aff33dd
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-10-11 19:29:59 +0000

    Token retrived in backend.

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,12 +5,15 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/sign_up">
+						<button className="btn btn-primary">Sign-Up</button>
+					</Link>
+				</div>
+
+				<div className="ml-auto">
+					<Link to="/login">
+						<button className="btn btn-primary">Login</button>
 					</Link>
 				</div>
 			</div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,13 +8,14 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>Welcome to my Simple Authentication App</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src="https://skytechgeek.com/wp-content/uploads/2019/06/full-stack-developer.jpg" />
 			</p>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
+
 			<p>
 				This boilerplate comes with lots of documentation:{" "}
 				<a href="https://start.4geeksacademy.com/starters/react-flask">
